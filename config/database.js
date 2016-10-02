6a119353c3bfc72857cf81e5d14237c23bd4e87a
acsant <acsant@uwaterloo.ca>
2016-10-01 21:04:25 -0400

    fix db configs

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'url': 'mongodb://<booksyncadmin>:<q7834kjwhekridjfo>@ds041939.mlab.com:41939/booksyncd'
+  'url': 'mongodb://booksyncadmin:q7834kjwhekridjfo@ds041939.mlab.com:41939/booksyncdb'
 };
