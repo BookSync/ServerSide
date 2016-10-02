de5443fbdfbf711451751e022c9474a2c0b5951a
acsant <acsant@uwaterloo.ca>
2016-10-01 20:52:51 -0400

    db configs

---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'googleAuth': {
+    'clientID' : '97881722410-2fb5hhjepugefqlck0tgnt65qgonbh44.apps.googleusercontent.com',
+    'clientSecret': 'Yfv8saildBSWF02K1Gcpx7FN',
+    'callbackURL' : 'http://localhost:8080/auth/google/callback'
+  }
+};
