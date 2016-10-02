08542ba818f1fdd7e2ce1e323e8ce0043817af7f
acsant <acsant@uwaterloo.ca>
2016-10-02 00:30:21 -0400

    bookmarks endpoint

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
-module.exports = function (app, passport) {
+var User = require('./models/user.js');
 
+module.exports = function (app, passport) {
   app.use(function(req, res, next) {
       res.header("Access-Control-Allow-Origin", "*");
       res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -23,6 +24,20 @@ module.exports = function (app, passport) {
           successRedirect: '/home',
           failureRedirect: '/'
         }));
+
+  app.get('/bookmarks', function (req, res) {
+    console.info(req.param('email'));
+    User.findOne({'google.email': req.param('email')}, function (err, user) {
+      if (err)
+        res.send(err);
+      if (user) {
+        res.send(user.bookmarks);
+      } else {
+        res.send("User not found");
+      }
+    });
+  });
+
 };
 
 function loggedIn(req, res, next) {
